60089364756e0f23d857a6f355968750e6155c6d
Mark Hegedus <mark@qualisync.com>
2024-09-12 01:56:22 +0200

    add basic actions, lot of issue in questionmark matching

--- index.ts
+++ index.ts
@@ -10,5 +10,85 @@ export class RegexMatcher {
     this.patternIndex = 0;
     this.textIndex = 0;
   }
+
+  match(text: string): boolean {
+    this.text = text;
+    this.patternIndex = 0;
+    this.textIndex = 0;
+    return this.matchHere();
+  }
+
+  private matchHere(): boolean {
+    if (this.patternIndex === this.pattern.length) {
+      return this.textIndex === this.text.length;
+    }
+
+    if (this.patternIndex + 1 < this.pattern.length) {
+      const nextChar = this.pattern[this.patternIndex + 1];
+      if (nextChar === "*") {
+        return this.matchStar(this.pattern[this.patternIndex]);
+      }
+      if (nextChar === "+") {
+        return this.matchPlus(this.pattern[this.patternIndex]);
+      }
+      if (nextChar === "?") {
+        return this.matchQuestion(this.pattern[this.patternIndex]);
+      }
+    }
+
+    if (
+      this.textIndex < this.text.length &&
+      (this.pattern[this.patternIndex] === "." ||
+        this.pattern[this.patternIndex] === this.text[this.textIndex])
+    ) {
+      this.patternIndex++;
+      this.textIndex++;
+      return this.matchHere();
+    }
+
+    return false;
+  }
+
+  private matchStar(c: string): boolean {
+    this.patternIndex += 2;
+    while (
+      this.textIndex < this.text.length &&
+      (c === "." || c === this.text[this.textIndex])
+    ) {
+      if (this.matchHere()) return true;
+      this.textIndex++;
+    }
+    return this.matchHere();
+  }
+
+  private matchPlus(c: string): boolean {
+    if (
+      this.textIndex >= this.text.length ||
+      (c !== "." && c !== this.text[this.textIndex])
+    ) {
+      return false;
+    }
+    this.patternIndex += 2;
+    this.textIndex++;
+    while (
+      this.textIndex < this.text.length &&
+      (c === "." || c === this.text[this.textIndex])
+    ) {
+      if (this.matchHere()) return true;
+      this.textIndex++;
+    }
+    return this.matchHere();
+  }
+
+  private matchQuestion(c: string): boolean {
+    this.patternIndex += 2;
+    if (
+      this.textIndex < this.text.length &&
+      (c === "." || c === this.text[this.textIndex])
+    ) {
+      this.textIndex++;
+    }
+    return this.matchHere();
+  }
 }
 
